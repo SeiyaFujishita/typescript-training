dfc19c3763d2046042e5bb3a45addb3531702b1a
daichinagano <d.nagano1002@gmail.com>
2021-11-30 08:02:42 +0900

    type alias

---
+++ src/type-aliases.ts
@@ -0,0 +1,18 @@
+export {};
+
+type Mojiretsu = string;
+
+const fooString: string = "Hello";
+const fooMojiretsu: Mojiretsu = "Hello";
+
+type Profile = {
+  name: string;
+  age: number;
+};
+
+const example1: Profile = {
+  name: "Ham",
+  age: 43,
+};
+
+type Profile2 = typeof example1;
